5bc566e0f97d270b01600c8db10d7c1f5767c832
Aman <aman@Amans-MacBook-Air.local>
2025-07-04 18:16:35 +0530

    FEAT: password saved as encrypted using bcrypt

--- src/config/server.config.js
+++ src/config/server.config.js
@@ -3,4 +3,5 @@ dotenv.config();
 
 module.exports = {
   PORT: process.env.PORT || 3000,
+  SALT_ROUNDS: process.env.SALT_ROUNDS || 8,
 };
--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,7 @@
 "use strict";
 const { Model } = require("sequelize");
+const bcrypt = require("bcrypt");
+const { serverConfig } = require("../config");
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     /**
@@ -34,5 +36,14 @@ module.exports = (sequelize, DataTypes) => {
       modelName: "User",
     }
   );
+
+  User.beforeCreate(function encrpt(user) {
+    const encrptedPassword = bcrypt.hashSync(
+      user.password,
+      +serverConfig.SALT_ROUNDS
+    );
+    user.password = encrptedPassword;
+  });
+
   return User;
 };
